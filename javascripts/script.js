2df58a40916a60c1b97128bdcbde699cbd2fcd92
RENAN DA COSTA ANDRADE <renan.candrade1@senacsp.edu.br>
2026-09-03 21:25:06 -0300

    atualização de pagina em js de contas

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,1 +1,15 @@
 document.getElementById("demo").innerHTML= "pirulito de maçã"
+let x, y, z;
+
+x= 5;
+y= 2;
+z= x*y;
+
+document.getElementById("resultado").innerHTML= z;
+
+let a,b;
+
+a=3;
+b=(100+50)*a;
+
+document.getElementById("resultado2").innerHTML= b;
